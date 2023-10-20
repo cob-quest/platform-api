eae978b909bab34649e03a652ee4f0ae6ea97424
soonann <sudo@soonann.dev>
2023-10-20 19:11:23 +0800

    fix: load secrets from secrets dir

--- configs/env.go
+++ configs/env.go
@@ -1,15 +1,20 @@
 package configs
 
 import (
+	"fmt"
 	"log"
 	"os"
+
 	"github.com/joho/godotenv"
 )
 
 func EnvMongoUri() string {
-	err := godotenv.Load()
+	err := godotenv.Load("./secrets/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	return os.Getenv("MONGOURI")	
+	MONGO_USER := os.Getenv("MONGO_USER")
+	MONGO_PASS := os.Getenv("MONGO_PASS")
+	MONGO_HOSTNAME := os.Getenv("MONGO_HOSTNAME")
+	return fmt.Sprintf("mongodb://%s,%s@%s:%s", MONGO_USER, MONGO_PASS, MONGO_HOSTNAME, "27017")
 }
