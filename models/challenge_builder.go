8e7675f1dea3a16ade40b113793fd93f2d34c355
Yao Long <yaolong.tan.2021@smu.edu.sg>
2023-10-22 02:03:46 +0800

    fix: camelcasing

--- models/challenge_builder.go
+++ models/challenge_builder.go
@@ -6,10 +6,10 @@ import (
 
 type ChallengeBuilder struct {
 	Id            primitive.ObjectID `json:"_id" bson:"_id"`
-	CorrelationID string             `json:"cor_id" bson:"cor_id"`
-	ChallengeName string             `json:"challengeName" bson:"challenge_name"`
-	CreatorName   string             `json:"creatorName" bson:"creator_name"`
-	S3Path        string             `json:"s3Path" bson:"s3_path"`
+	CorID 		  string             `json:"corId" bson:"corId"`
+	ChallengeName string             `json:"challengeName" bson:"challengeName"`
+	CreatorName   string             `json:"creatorName" bson:"creatorName"`
+	S3Path        string             `json:"s3Path" bson:"s3Path"`
 }
 
 type ChallengeBuilderList struct {
--- models/process.go
+++ models/process.go
@@ -6,12 +6,12 @@ import (
 
 type Process struct {
 	Id            primitive.ObjectID `json:"_id" bson:"_id"`
-	CorrelationID string             `json:"cor_id" bson:"cor_id"`
-	EventSuccess  bool               `json:"eventSuccess" bson:"event_success"`
-	ImageName     string             `json:"imageName" bson:"image_name"`
-    CreatorName   string             `json:"creatorName" bson:"creator_name"`
-    S3path        string             `json:"s3Path" bson:"s3_path"`
-    Timestamp     primitive.DateTime `json:"timeStamp" bson:"timestamp"`
+	CorId 		  string             `json:"corId" bson:"corId"`
+	EventSuccess  bool               `json:"eventSuccess" bson:"eventSuccess"`
+	ImageName     string             `json:"imageName" bson:"imageName"`
+    CreatorName   string             `json:"creatorName" bson:"creatorName"`
+    S3Path        string             `json:"s3Path" bson:"s3Path"`
+    Timestamp     primitive.DateTime `json:"timestamp" bson:"timestamp"`
 }
 
 type ProcessList struct {
